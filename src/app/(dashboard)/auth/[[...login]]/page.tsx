ad0abf59aedcd5c5509f4e8926046daad06078d4
“Jakkrit.O” <jakkrit.o@gli.co.th>
2026-02-13 20:00:09 +0700

    fixed router.replace

--- src/app/(dashboard)/auth/[[...login]]/page.tsx
+++ src/app/(dashboard)/auth/[[...login]]/page.tsx
@@ -16,10 +16,7 @@ const LoginPage = () => {
   const router = useRouter();
 
   const { data: session, status } = useSession();
-  if (session && status === 'authenticated') {
-    router.replace('/dashboard');
-  }
-
+  
   const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
